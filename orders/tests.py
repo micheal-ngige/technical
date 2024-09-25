3a8a883f62eba5d93da1056031367f46b0045918
michael <michaelngige@gmail.com>
2024-09-25 10:13:22 +0300

    debugging for GAE authentication in .env file

--- orders/tests.py
+++ orders/tests.py
@@ -3,14 +3,14 @@ from .models import Customer, Order
 
 class CustomerModelTest(TestCase):
     def setUp(self):
-        self.customer = Customer.objects.create(name="Mike Ngige", code="MN123")
+        self.customer = Customer.objects.create(name="John Doe", code="JD123")
 
     def test_customer_creation(self):
-        self.assertEqual(self.customer.name, "Mike Ngige")
+        self.assertEqual(self.customer.name, "John Doe")
 
 class OrderModelTest(TestCase):
     def setUp(self):
-        customer = Customer.objects.create(name="Mike Ngige" code="MN123")
+        customer = Customer.objects.create(name="John Doe", code="JD123")
         self.order = Order.objects.create(customer=customer, item="Laptop", amount=1000)
 
     def test_order_creation(self):
